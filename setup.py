387665fd7f3ffdac5ce6d76f0680e33014c2b233
Dan Strohl <dstrohl@users.noreply.github.com>
2021-09-23 14:51:21 -0700

    added empty requirements.txt file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AdvancedCounter',
+    version='1.0',
+    packages=['advanced_counter', 'advanced_counter.tests'],
+    url='https://github.com/dstrohl/AdvancedCounter',
+    license='GNU 3.0',
+    author='Dan Strohl',
+    author_email='dan@wjcg.net',
+    description='Advanced Counting helper'
+)
